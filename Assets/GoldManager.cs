a9f75767355a8c00467b26f6032d785787a32479
RyanRoberts96 <ryanroberts1996@protonmail.com>
2025-04-28 14:52:45 +0100

    Added gold display

--- Assets/GoldManager.cs
+++ Assets/GoldManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GoldManager : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class GoldManager : MonoBehaviour
     public static GoldManager Instance;
 
     public int totalGold = 0;
+
+    public TextMeshProUGUI goldText;
 
     private void Awake()
     {
@@ -21,10 +24,24 @@ public class GoldManager : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        UpdateGoldUI();
+    }
+
+
     public void AddGold(int amount)
     {
         totalGold += amount;
         Debug.Log("Total gold: " + totalGold);
+        UpdateGoldUI();
     }
 
+    public void UpdateGoldUI()
+    {
+        if (goldText != null)
+        {
+            goldText.text = "Gold: " + totalGold;
+        }
+    }
 }
